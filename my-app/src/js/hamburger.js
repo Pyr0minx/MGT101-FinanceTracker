34ed150243d1892bf27b78ab51baefd5ee1d4880
Aiden Hicks <aiden.hicks.anhd@gmail.com>
2026-07-23 01:27:48 -0600

    Added Responsiveness and Hamburger Menu to template

---
+++ my-app/src/js/hamburger.js
@@ -0,0 +1,15 @@
+let menuButton = document.getElementById('hamburger-icon');
+let menu = document.getElementById('header-links');
+let menuActive = false;
+
+function toggleMobileMenu() {
+    if (menuActive) {
+        menu.style.display = 'none';
+        menuActive = false;
+    } else {
+        menu.style.display = 'flex';
+        menuActive = true;
+    }
+}
+
+menuButton.addEventListener('click', e => {toggleMobileMenu()})
